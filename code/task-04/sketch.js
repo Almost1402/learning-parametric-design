e25937d60118c07799d98af299fc32611aa7a047
Almost1402 <103105641+Almost1402@users.noreply.github.com>
2022-04-27 10:04:32 +0200

    Create sketch.js

---
+++ code/task-04/sketch.js
@@ -0,0 +1,35 @@
+function preload(){
+  // preload assets
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill (255);
+  triangle(200,0,330,0,200,200);
+  fill (50);
+  triangle(200,150,232,150,200,200);
+
+  fill (255);
+  triangle(330,200,330,400,460,400);
+  fill (50);
+  triangle(330,200,362,250,330,250);
+
+  fill (255);
+  triangle(60,200,60,400,200,400);
+  fill (50);
+  triangle(60,200,60,250,95,250);
+
+  fill (255);
+  rect (0,0,60,150);
+
+  fill (255);
+  rect (200,250,130,150)
+
+ 
+      
+
+}
